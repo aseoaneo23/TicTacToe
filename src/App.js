c65632e8e180f15211ed57d0063ca2a612fc920c
anseoane1 <anseoane1@gmai.com>
2023-11-28 23:18:09 +0100

    Parte 7: Levantamos estados para almacenar movimientos en un historial

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ const Square=({value, onSquareClick})=>{
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
+function Board({ xIsNext, squares, onPlay }) {
   const handleClick=(i)=>{
 
     if (squares[i] || calculateWinner(squares)) {
@@ -21,8 +18,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   const winner = calculateWinner(squares);
   let status;
@@ -51,6 +47,28 @@ export default function Board() {
       <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
     </div>
     </>
+  );
+}
+
+export default function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+const handlePlay=(nextSquares)=> {
+  setHistory([...history, nextSquares]);
+  setXIsNext(!xIsNext);
+}
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
